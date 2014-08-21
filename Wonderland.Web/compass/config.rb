5db645cefcfdde11a3d2de69d6ed4f9471620455
Sam Latif <mail@samlatif.co.uk>
2014-08-21 16:52:14 +0100

    Successfuly intergrated (and bug fixed 3rd party Compasss extensions) sass-boilerplate

---
+++ Wonderland.Web/compass/config.rb
@@ -0,0 +1,15 @@
+# Compass configuration file.
+
+# Require any additional compass plugins here.
+require "susy"
+
+project_path =File.expand_path('..',File.dirname(__FILE__))
+
+# Important! change the paths below to match your project setup
+css_dir = "css" # update to the path of your css files.
+sass_dir = "css/sass" # update to the path of your sass files.
+images_dir = "img" # update to the path of your image files.
+javascripts_dir = "scripts" # update to the path of your script files.
+
+line_comments = true # if debugging (using chrome extension - set this to true)
+cache = true
