61c354a683e262af133115642440a467a4beb059
Hendy Racher <git@hendyracher.co.uk>
2014-08-21 16:21:35 +0100

    added PartyIdeaTileSize enum (for use with nuPicker RadioButton Picker)

---
+++ Wonderland.Logic/Enums/PartyIdeaTileSize.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Wonderland.Logic.Enums
+{
+    using System;
+
+    /// <summary>
+    /// Used within Umbraco to specify the the size of a Party Idea Tile
+    /// </summary>
+    public enum PartyIdeaTileSize
+    {
+        /// <summary>
+        /// Large
+        /// </summary>
+        Large,
+
+        /// <summary>
+        /// Medium
+        /// </summary>
+        Medium,
+
+        /// <summary>
+        /// Small
+        /// </summary>
+        Small
+    }
+}
