a610b6571668c2951414eaee7ba03ecd1cf06b4b
Hendy Racher <git@hendyracher.co.uk>
2014-08-22 00:42:40 +0100

    added Home model properties for BannerHeading and BannerCopy

--- Wonderland.Logic/Models/Content/Home.cs
+++ Wonderland.Logic/Models/Content/Home.cs
@@ -1,12 +1,29 @@
 ﻿
 namespace Wonderland.Logic.Models.Content
 {
+    using Umbraco.Web;
     using Umbraco.Core.Models;
 
     public class Home : SitePage
     {
         public Home(IPublishedContent content) : base(content)
         {
+        }
+
+        public string BannerHeading
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("bannerHeading");
+            }
+        }
+
+        public string BannerCopy
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("bannerCopy");
+            }
         }
     }
 }
--- Wonderland.Logic/Models/Content/SitePage.cs
+++ Wonderland.Logic/Models/Content/SitePage.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace Wonderland.Logic.Models.Content
 {
+    using Umbraco.Web;
     using Umbraco.Core.Models;
     using Umbraco.Core.Models.PublishedContent;
 
@@ -8,6 +9,14 @@ namespace Wonderland.Logic.Models.Content
     {
         public SitePage(IPublishedContent content) : base(content)
         {
+        }
+
+        public string MetaDescription
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("metaDescription");
+            }
         }
     }
 }
