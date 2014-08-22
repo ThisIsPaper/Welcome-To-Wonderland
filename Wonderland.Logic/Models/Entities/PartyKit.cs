e82bf2b6871dc8c964e55daa90bee988a2b2ff9c
Hendy Racher <git@hendyracher.co.uk>
2014-08-22 22:18:49 +0100

    skeleton model for Image

--- Wonderland.Logic/Models/Entities/PartyKit.cs
+++ Wonderland.Logic/Models/Entities/PartyKit.cs
@@ -1,21 +1,26 @@
 ﻿
 namespace Wonderland.Logic.Models.Entities
 {
+    using System.Linq;
+    using System.Collections.Generic;
+    using Wonderland.Logic.Models.Media;
     using Archetype.Models;
+    using Umbraco.Core.Models;
+    using System.Web.WebPages;
 
     public class PartyKit
     {
         internal PartyKit(ArchetypeFieldsetModel archetypeFieldsetModel)
         {
-            this.Heading = archetypeFieldsetModel.GetValue<string>("heading");
-
+            this.Heading = archetypeFieldsetModel.GetValue<string>("heading");                        
+            //this.Images = archetypeFieldsetModel.GetValue<IEnumerable<IPublishedContent>>("images").Select(x => new Image(x));
             this.LinkText = archetypeFieldsetModel.GetValue<string>("linkText");
             this.Copy = archetypeFieldsetModel.GetValue<string>("copy");
         }
 
         public string Heading { get; internal set; }
 
-        // Images
+        public IEnumerable<Image> Images { get; internal set;}
 
         public string LinkText { get; internal set; }
 
---
+++ Wonderland.Logic/Models/Media/Image.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Wonderland.Logic.Models.Media
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Umbraco.Web;
+    using Umbraco.Core.Models;
+    using nuPickers;
+    using Umbraco.Core.Models.PublishedContent;
+
+    public class Image : PublishedContentModel
+    {
+        public new const string Alias = "Image";
+
+        public Image(IPublishedContent content)
+            : base(content)
+        {
+        }
+
+        public string Url
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("umbracoFile");
+            }
+        }
+
+        public string HtmlAltText
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("htmlAltText");
+            }
+        }
+
+    }
+}
