ea6f90c7c5f16f3c0ed46bc7dac2c8a1ce548fb4
Hendy Racher <git@hendyracher.co.uk>
2014-08-24 11:01:01 +0100

    added empty model for Partier (will use a single MemberType)

---
+++ Wonderland.Logic/Models/Members/Partier.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Wonderland.Logic.Models.Members
+{
+    using Umbraco.Core.Models;
+    using Umbraco.Core.Models.PublishedContent;
+
+    public class Partier : PublishedContentModel
+    {
+        public const string Alias = "Partier";
+
+        public Partier(IPublishedContent content)
+            : base(content)
+        {
+        }
+    }
+}
