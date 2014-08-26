1b77fc31547d068d7097ff3849613a7d598ed935
Hendy Racher <git@hendyracher.co.uk>
2014-08-26 21:33:14 +0100

    removed reference to crop urls

--- Wonderland.Logic/Models/Entities/Reward.cs
+++ Wonderland.Logic/Models/Entities/Reward.cs
@@ -1,20 +1,23 @@
 ﻿
 namespace Wonderland.Logic.Models.Entities
 {
+    using Wonderland.Logic.Models.Media;
     using Archetype.Models;
+    using Umbraco.Core.Models;
 
     public class Reward
     {
         internal Reward(ArchetypeFieldsetModel archetypeFieldsetModel)
         {
             this.Heading = archetypeFieldsetModel.GetValue<string>("heading");
+            this.Image = new Image(archetypeFieldsetModel.GetValue<IPublishedContent>("image"));
             this.SubHeading = archetypeFieldsetModel.GetValue<string>("subHeading");
             this.Copy = archetypeFieldsetModel.GetValue<string>("copy");
         }
 
         public string Heading { get; internal set; }
 
-        // Image
+        public Image Image { get; internal set; }
 
         public string SubHeading { get; internal set; }
 
