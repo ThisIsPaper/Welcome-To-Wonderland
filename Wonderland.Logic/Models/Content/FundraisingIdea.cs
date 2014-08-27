71940d4bfd93160f07421f0d48bdeed296ec3b73
Hendy Racher <git@hendyracher.co.uk>
2014-08-27 13:23:30 +0100

    fleshed out FAQs model

--- Wonderland.Logic/Models/Content/FundraisingIdea.cs
+++ Wonderland.Logic/Models/Content/FundraisingIdea.cs
@@ -2,6 +2,8 @@
 namespace Wonderland.Logic.Models.Content
 {
     using Umbraco.Core.Models;
+    using Umbraco.Web;
+    using Wonderland.Logic.Models.Media;
 
     public class FundraisingIdea : SitePage
     {
@@ -10,6 +12,30 @@ namespace Wonderland.Logic.Models.Content
         public FundraisingIdea(IPublishedContent content)
             : base(content)
         {
+        }
+
+        public string Heading
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("heading");
+            }
+        }
+
+        public Image Image
+        {
+            get
+            {
+                return new Image(this.GetPropertyValue<IPublishedContent>("image"));
+            }
+        }
+
+        public string Copy
+        {
+            get
+            {
+                return this.GetPropertyValue<string>("copy");
+            }
         }
     }
 }
