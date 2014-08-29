d7714078ce55a5e50d556ed105aa9193e8010d32
Sam Latif <mail@samlatif.co.uk>
2014-08-29 17:39:03 +0100

    Styling touches on home page and began work on expanding/compressing header

--- Wonderland.Web/scripts/main.js
+++ Wonderland.Web/scripts/main.js
@@ -1,1 +1,15 @@
+$(document).ready( function() {
 
+    window.onscroll = function(e) {
+        $scroll = $(window).scrollTop();
+        $siteHeader = $('#siteHeader');
+
+        if ( $scroll > 250 ) {
+            $siteHeader.addClass('compressed');
+        }
+        else {
+            $siteHeader.removeClass('compressed');
+        }
+    }
+
+});
