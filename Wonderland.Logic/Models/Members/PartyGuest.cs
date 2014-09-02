0705d214e48a4e3b541a7bed50984bb61b3adbac
Hendy Racher <git@hendyracher.co.uk>
2014-09-02 22:56:58 +0100

    skeleton model for PartyGuest

---
+++ Wonderland.Logic/Models/Members/PartyGuest.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Wonderland.Logic.Models.Members
+{
+    using System.Web.Security;
+    using Umbraco.Core.Models;
+    using Umbraco.Web;
+    using Umbraco.Web.Security;
+    using Wonderland.Logic.Models.Entities;
+
+    public class PartyGuest : BaseMemberType
+    {
+        // Member Type
+        public const string Alias = "PartyGuest";
+
+        // Properties
+
+        public PartyGuest(IPublishedContent content)
+            : base(content)
+        {
+        }
+
+        public static explicit operator PartyGuest(MembershipUser membershipUser)
+        {
+            return new PartyGuest(new MembershipHelper(UmbracoContext.Current).GetByUsername(membershipUser.UserName));
+        }
+    }
+}
