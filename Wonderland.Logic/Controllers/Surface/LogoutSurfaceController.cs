e48b97121e764fc5e0b7fb6c3501e613824a37f3
Hendy Racher <git@hendyracher.co.uk>
2014-09-02 14:28:28 +0100

    wired up a logout link

---
+++ Wonderland.Logic/Controllers/Surface/LogoutSurfaceController.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Wonderland.Logic.Controllers.Surface
+{
+    using System.Web.Mvc;
+    using System.Web.Security;
+    using Umbraco.Web.Mvc;
+
+    public class LogoutSurfaceController : SurfaceController
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            return this.Redirect("/");
+        }
+    }
+}
+
