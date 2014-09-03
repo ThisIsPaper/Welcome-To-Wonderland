237a4f1c9556b6a6fe5ef6eea109dee13d2569c7
Hendy Racher <git@hendyracher.co.uk>
2014-09-03 11:57:01 +0100

    added custom 404 docType / view / model / controller

---
+++ Wonderland.Logic/Events/SetPageNotFoundHandler.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Wonderland.Logic.Events
+{
+    using Umbraco.Core;
+    using Umbraco.Core.Models.PublishedContent;
+    using Umbraco.Web.Routing;
+    using Wonderland.Logic.Models.Content;
+
+    public class SetPageNotFoundHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentLastChanceFinderResolver.Current.SetFinder(new PageNotFoundHandler());
+        }
+    }
+
+    public class PageNotFoundHandler : IContentFinder
+    {
+        public bool TryFindContent(PublishedContentRequest contentRequest)
+        {
+            if (contentRequest.Is404)
+            {
+                // get the "Page Not Found" page
+                contentRequest.PublishedContent = contentRequest.RoutingContext.UmbracoContext.ContentCache.GetSingleByXPath("//" + PageNotFound.Alias);
+
+                contentRequest.SetResponseStatus(404);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
