2636607942db6ae7c07d27654162f7ab0e593198
Hendy Racher <git@hendyracher.co.uk>
2014-09-04 13:36:10 +0100

    added controllers for the decoration / fashion / playlist tiles to redirect back to their partent

---
+++ Wonderland.Logic/Controllers/Render/FashionTileController.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Wonderland.Logic.Controllers.Render
+{
+    using System.Web.Mvc;
+
+    public class FashionTileController : BaseRenderMvcController
+    {
+        public override ActionResult Index(Umbraco.Web.Models.RenderModel model)
+        {
+            // always redirect to partent
+            return this.Redirect(model.Content.Parent.Url);
+        }
+    }
+}
