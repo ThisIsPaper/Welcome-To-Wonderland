8102a140de5e92e1251d290b7ce19b6e46142990
Hendy Racher <git@hendyracher.co.uk>
2014-09-11 00:42:07 +0100

    added MembershipHelper extensions

---
+++ Wonderland.Logic/Extensions/MembershipHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Wonderland.Logic.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Umbraco.Web.Security;
+    using Wonderland.Logic.Enums;
+    using Wonderland.Logic.Models.Members;
+
+    public static class MembershipHelperExtensions
+    {
+        public static IEnumerable<IPartier> GetPartiers(this MembershipHelper membershipHelper, Guid partyGuid)
+        {
+            return Enumerable.Empty<IPartier>();
+        }
+
+        public static IEnumerable<PartyHost> GetTopPartyHosts(this MembershipHelper membershipHelper, LeaderboardType leaderboardType, int take)
+        {
+            return Enumerable.Empty<PartyHost>();
+        }
+
+    }
+}
