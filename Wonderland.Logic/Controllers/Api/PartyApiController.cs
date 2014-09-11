064edb4049e17d5a348e8a3339727c0466be2660
Hendy Racher <git@hendyracher.co.uk>
2014-09-11 13:53:54 +0100

    Api Controller POC

---
+++ Wonderland.Logic/Controllers/Api/PartyApiController.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Wonderland.Logic.Controllers.Api
+{
+    using Newtonsoft.Json.Linq;
+    using System;
+    using System.Web.Http;
+    using Umbraco.Web.WebApi;
+    using Wonderland.Logic.Models.Members;
+
+    [MemberAuthorize(AllowType=PartyHost.Alias)]
+    public class PartyApiController : UmbracoApiController
+    {
+        // ~/Umbraco/Api/PartyApi/UpdatePartyCopy?partyGuid=****
+        [HttpPost]
+        public bool UpdatePartyCopy([FromUri] Guid partyGuid, [FromBody] JObject data)
+        {
+            var partyCopy = data.GetValue("partyCopy");
+
+            return false;
+        }
+    }
+}
