2b3a14ac151eb1b29614ddc7c3e2f57728bcb3bc
Hendy Racher <git@hendyracher.co.uk>
2014-09-16 18:34:49 +0100

    setting default post header

--- Wonderland.Web/scripts/app/app.js
+++ Wonderland.Web/scripts/app/app.js
@@ -1,1 +1,5 @@
 var wonderlandApp = angular.module('wonderlandApp', ['oc.modal']);
+
+wonderlandApp.config(function ($httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+});
