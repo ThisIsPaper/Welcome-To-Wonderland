975364fad15dbaa2868147b1b7c10535b9d99be6
MattRawson <rawson.matt@googlemail.com>
2014-09-18 23:31:49 +0100

    - start of wall, but it seems to error on post

--- Wonderland.Web/scripts/app/controllers/wallCtrl.js
+++ Wonderland.Web/scripts/app/controllers/wallCtrl.js
@@ -1,7 +1,46 @@
-wonderlandApp.controller('WallCtrl', ['$scope', function ($scope) {
+wonderlandApp.controller('WallCtrl', ['mHttp', '$scope', function (mHttp, $scope) {
 
-    $scope.init = function (feedBaseUrl, partyGUID) {
-        console.log(feedBaseUrl, partyGUID);
+    var wallFeed, partyGuid, feedRequest;
+
+    $scope.wall = {
+        feed: null,
+        formModel: null
     };
+
+    $scope.init = function (baseWallFeed, basePartyGuid) {
+        wallFeed = baseWallFeed;
+        partyGuid = basePartyGuid;
+
+        $scope.getFeed();
+    };
+
+
+    $scope.getFeed = function () {
+
+        console.log('GET FEED!');
+
+        feedRequest = mHttp.get(wallFeed, {
+            data: {
+                'partyGuid': partyGuid,
+                'take': 10
+            },
+            dataType: 'json'
+        });
+
+        feedRequest.then(function (response) {
+            console.log('Wall::feed', response);
+            $scope.wall.feed = response;
+        });
+
+    };
+
+    $scope.wallFormModelInit = function (m) {
+        console.log('wallModelInit', m);
+        $scope.wall.formModel = m;
+    };
+
+    $scope.$onRootScope('wallMessagePosted', function(event, response, dataObject) {
+        console.log('wallMessagePosted', response, dataObject);
+    });
 
 }]);
