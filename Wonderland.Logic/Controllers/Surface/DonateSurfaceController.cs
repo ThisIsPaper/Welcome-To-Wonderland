7f80229956f2775507bbbd9f6d4fbeac7c46d02b
Hendy Racher <git@hendyracher.co.uk>
2014-09-18 22:02:36 +0100

    added donate link to the party views

---
+++ Wonderland.Logic/Controllers/Surface/DonateSurfaceController.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Wonderland.Logic.Controllers.Surface
+{
+    using System;
+    using System.Web.Mvc;
+    using Umbraco.Web.Mvc;
+    using Wonderland.Logic.Models.Content;
+
+    public class DonateSurfaceController : SurfaceController
+    {
+        /// <summary>
+        /// Handles inbound links to the donate page
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult NavigateToDonateUrl(Guid partyGuid)
+        {
+            return this.Redirect(this.Umbraco.TypedContentSingleAtXPath("//" + Donate.Alias).Url + "?partyGuid=" + partyGuid.ToString());
+        }
+    }
+}
