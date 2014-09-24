aa76c1edc1e9cf6ca282c5a3c3d1c467763edfc1
MattRawson <rawson.matt@googlemail.com>
2014-09-24 18:51:52 +0100

    - mostly wonderlanders

--- Wonderland.Web/scripts/app/filters/all.js
+++ Wonderland.Web/scripts/app/filters/all.js
@@ -15,3 +15,25 @@ wonderlandApp.filter('datePreviewFormat', [function () {
     };
 
 }]);
+
+
+wonderlandApp.filter('dateSuffix', function() {
+
+    var suffixes = ['st', 'nd', 'rd'];
+
+    return function(num) {
+        var suffix = '';
+        if (angular.isNumber(num) && num>0) {
+            var ind = [1, 2, 3].indexOf(num % 10);
+            var lastTwoDigits = num % 100;
+
+            if (ind >=0 && !(lastTwoDigits>10 && lastTwoDigits<20)) {
+                suffix = suffixes[ind];
+            } else {
+                suffix = 'th';
+            }
+        }
+
+        return num+suffix;
+    };
+});
