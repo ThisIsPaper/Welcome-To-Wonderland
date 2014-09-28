15f067e245c2b55a72659fe70c73e239ccba8d02
MattRawson <rawson.matt@googlemail.com>
2014-09-28 14:57:04 +0100

    - price formatting

--- Wonderland.Web/scripts/app/filters/all.js
+++ Wonderland.Web/scripts/app/filters/all.js
@@ -43,3 +43,30 @@ wonderlandApp.filter('escape', function () {
         return encodeURIComponent(url);
     };
 });
+
+
+wonderlandApp.filter('percentageRounder', function () {
+    return function (current, total) {
+        var p = 0;
+
+        if (angular.isNumber(current) && angular.isNumber(total)) {
+            p = Math.floor((current/total)*100);
+            p = p > 100 ? 100 : p;
+            p = p < 0 ? 0 : p;
+        }
+        return p;
+    };
+});
+
+
+wonderlandApp.filter('mCurrency', ["$filter", function ($filter) {
+    return function(amount, currencySymbol){
+        var currency = $filter('currency');
+
+        if(amount < 0){
+            return currency(amount, currencySymbol).replace("(", "-").replace(")", "");
+        }
+
+        return currency(amount, currencySymbol);
+    };
+}]);
