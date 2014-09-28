90aaeb03a29e2b190e1c25e3af37fedeffff0638
Hendy Racher <git@hendyracher.co.uk>
2014-09-28 18:02:01 +0100

    added My Profile page in cms

---
+++ Wonderland.Logic/Models/Content/MyProfile.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Wonderland.Logic.Models.Content
+{
+    using Umbraco.Core.Models;
+    using Umbraco.Web;
+
+    public class MyProfile : SitePage
+    {
+        // Document Type
+        public new const string Alias = "MyProfile";
+
+        public MyProfile(IPublishedContent content)
+            : base(content)
+        {
+        }
+    }
+}
