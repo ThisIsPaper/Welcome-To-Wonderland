1fa31b0492b1d5a28317602f6f6f5e0c74504032
MattRawson <rawson.matt@googlemail.com>
2014-09-30 23:00:08 +0100

    - time picker

--- Wonderland.Web/scripts/app/controllers/partyDetailsFormCtrl.js
+++ Wonderland.Web/scripts/app/controllers/partyDetailsFormCtrl.js
@@ -6,7 +6,21 @@ TODO - NEED THIS TO SPLIT OUT DATE AND TIME
 
 wonderlandApp.controller('PartyDetailsFormCtrl', ['$scope', function ($scope) {
 
-    var hasSetInitialDate = false;
+    var hasSetInitialDate = false,
+        minute = [0, 30];
+
+    /**
+     *
+     * CREATE OPTIONS FOR TIME
+     */
+    $scope.times = [];
+    for (var i=0; i<24; i++) {
+        angular.forEach(minute, function (valueMinute) {
+            $scope.times.push((i<10?'0':'') + i + ":" + (valueMinute<10?'0':'') + valueMinute);
+        });
+    };
+
+
 
     $scope.tempModel = {
         PartyDateTime: null
