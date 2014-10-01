9092fe71021b8c9d037d90013a3508967ff691bf
MattRawson <rawson.matt@googlemail.com>
2014-10-01 23:12:11 +0100

    - eurgh hate the window click toggle

---
+++ Wonderland.Web/scripts/app/directives/mWindowClickToggle.js
@@ -0,0 +1,27 @@
+wonderlandApp.directive('mWindowClickToggle', ['safeApply', '$document', function (safeApply, $document) {
+
+    return {
+        scope: {
+            toggle: '=mWindowClickToggle'
+        },
+        link: function (scope, element) {
+
+            scope.toggle = false;
+
+            $document.bind('click', function(event){
+                var isClickedElementChildOfPopup = element
+                    .find(event.target)
+                    .length > 0;
+
+                if (isClickedElementChildOfPopup)
+                    return;
+
+                safeApply(scope, function () {
+                    scope.toggle = false;
+                });
+            });
+
+        }
+    }
+
+}]);
