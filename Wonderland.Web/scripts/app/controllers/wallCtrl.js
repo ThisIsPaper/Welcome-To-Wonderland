91ba24c40deff8fd1b8fc8eb7efcd6034d5a5629
MattRawson <rawson.matt@googlemail.com>
2014-10-01 11:44:36 +0100

    - nearly finished image posting on walls

--- Wonderland.Web/scripts/app/controllers/wallCtrl.js
+++ Wonderland.Web/scripts/app/controllers/wallCtrl.js
@@ -1,10 +1,19 @@
-wonderlandApp.controller('WallCtrl', ['mHttp', '$filter', '$scope', function (mHttp, $filter, $scope) {
+wonderlandApp.controller('WallCtrl', ['mHttp', 'safeApply', '$filter', '$scope', '$timeout', function (mHttp, safeApply, $filter, $scope, $timeout) {
 
-    var wallFeed, partyGuid, feedRequest;
+    var wallFeed, partyGuid, feedRequest, initialFormModel, hasSetInitialFormModel=false;
 
     $scope.wall = {
+        feedback: {
+            imageProcessing: false,
+            imageShowSuccess: false,
+            imageShowError: false,
+
+            messageProcessing: false
+        },
+
         feed: null,
         formModel: null,
+        previewImageUrl: null,
         hasDoneFirstLoad: false
     };
 
@@ -36,9 +45,14 @@ wonderlandApp.controller('WallCtrl', ['mHttp', '$filter', '$scope', function (mH
                         value.wallPostType = "Donation";
                         value.text = $filter('mCurrency')(value.text, '£');
                     }
-                    value.timestamp = moment(value.timestamp).fromNow();
+                    // TODO: need to remove this hardcoded time subtraction, server times differ, must be timezone issues
+                    value.timestamp = moment(value.timestamp).subtract(1, 'hour').fromNow();
+
+                    value.imageUrl = value.imageUrl.indexOf('null') >= 0 ? null : value.imageUrl;
                 });
             }
+
+            console.log('FEED', response);
 
             $scope.wall.hasDoneFirstLoad = true;
             $scope.wall.feed = response;
@@ -47,23 +61,59 @@ wonderlandApp.controller('WallCtrl', ['mHttp', '$filter', '$scope', function (mH
     };
 
     $scope.wallFormModelInit = function (formModel) {
-        $scope.wall.formModel = formModel;
+
+        if (!hasSetInitialFormModel) {
+            hasSetInitialFormModel = true;
+            initialFormModel = formModel;
+        }
+
+        safeApply($scope, function () {
+            $scope.wall.formModel = angular.copy(formModel);
+
+            $scope.wall.previewImageUrl = null;
+            $scope.wall.feedback.imageProcessing = false;
+            $scope.wall.feedback.messageProcessing = false;
+        });
     };
 
-    $scope.$onRootScope('wallMessagePosted', function(event, response, dataObject) {
-        $scope.wallFormModelInit();
+    $scope.$onRootScope('wallMessagePosted', function() {
+        $scope.wallFormModelInit(initialFormModel);
         $scope.getFeed();
     });
 
+    $scope.$onRootScope('wallImageUploadStart', function() {
+        safeApply($scope, function () {
+            $scope.wall.previewImageUrl = null;
+            $scope.wall.formModel.PartyWallImage = null;
+            $scope.wall.feedback.imageProcessing = true;
+        });
+    });
+
     $scope.$onRootScope('wallImageUrlUploaded', function(event, response) {
-        if (response && response.Success === true && response.Message) {
 
-            var url = response.Message;
+        /**
+         * safe apply the feedback response
+         */
+        safeApply($scope, function () {
+            $scope.wall.feedback.imageProcessing = false;
 
-            console.log('wallImageUrlUploaded', url);
+            if (response && response.Success === true && response.Message) {
+                $scope.wall.feedback.imageShowSuccess = true;
 
-            $scope.wall.formModel.PartyWallImage = url;
-        }
+                // TODO: are we leaving this as is. i.e. returning the full uri of image location an javascript stripping out just the guid to save?
+
+                $scope.wall.previewImageUrl = response.Message;
+                $scope.wall.formModel.PartyWallImage = $scope.wall.previewImageUrl.replace('/Uploads/PartyWall/', '');
+
+            } else {
+                $scope.wall.feedback.imageShowError = true;
+            }
+
+            $timeout(function () {
+                $scope.wall.feedback.imageShowSuccess = false;
+                $scope.wall.feedback.imageShowError = false;
+            }, 5000);
+        });
     });
 
 }]);
