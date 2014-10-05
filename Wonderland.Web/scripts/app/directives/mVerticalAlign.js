2e9f74a0ab19633f1e38305aa2fc91c3a512eafd
MattRawson <rawson.matt@googlemail.com>
2014-10-05 15:04:30 +0100

    - party ideas category page back button

--- Wonderland.Web/scripts/app/directives/mVerticalAlign.js
+++ Wonderland.Web/scripts/app/directives/mVerticalAlign.js
@@ -7,7 +7,6 @@ wonderlandApp.directive('mVerticalAlign', ['debounce', '$timeout', '$window', fu
             var hasInited = false,
                 myParent = angular.element(element).parent(),
                 reAlign = debounce(function () {
-                    console.log(scope.parentHeight(), element.height(), (scope.parentHeight() - element.height())/2);
                     element.css('top', (scope.parentHeight() - element.height())/2);
                 }, 200);
 
@@ -15,12 +14,11 @@ wonderlandApp.directive('mVerticalAlign', ['debounce', '$timeout', '$window', fu
             scope.parentWidth = function () { return myParent.width(); };
 
             attrs.$observe('mVerticalAlign', function (newVal) {
-                console.log('mVerticalAlign', newVal, newVal==true);
-                if (!hasInited && newVal===true) {
+                if (!hasInited && !!newVal===true) {
 
                     scope.$watch('parentHeight() + parentWidth()', reAlign);
                     angular.element($window).bind('resize', reAlign);
-console.log('GO!');
+
                     reAlign();
                     $timeout(reAlign, 500);
                 }
