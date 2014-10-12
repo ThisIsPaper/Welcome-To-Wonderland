64f36a9556eeb192cadc9364ee361a6ea2c24b6f
MattRawson <rawson.matt@googlemail.com>
2014-10-12 15:09:22 +0100

    remove current profile image, frontend working but backend doesn't handle the null object properly. Hendy needs to fix this.

--- Wonderland.Web/scripts/app/directives/mImageLoadedNotifier.js
+++ Wonderland.Web/scripts/app/directives/mImageLoadedNotifier.js
@@ -4,6 +4,7 @@ wonderlandApp.directive('mImageLoadedNotifier', [function () {
         restrict: 'A',
         link: function (scope, element) {
             angular.element(element).bind('load', function () {
+                angular.element(element).unbind('load');
                 scope.$emit('childImageLoaded');
             });
         }
