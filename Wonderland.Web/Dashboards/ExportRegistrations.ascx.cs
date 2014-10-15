1a39cc7e7dcc2608f46ef6aa415c359da77eedf2
Hendy Racher <git@hendyracher.co.uk>
2014-10-15 10:03:04 +0100

    fix for csv exports + binaries

--- Wonderland.Web/Dashboards/ExportRegistrations.ascx.cs
+++ Wonderland.Web/Dashboards/ExportRegistrations.ascx.cs
@@ -34,36 +34,36 @@ namespace Wonderland.Web.Dashboards
                         string.Join(
                             ", ",
                             new object[] { 
-                                partier.Id,                                                                             // 1.  Member Id
-                                PartyHost.Alias,                                                                        // 2.  Member Type
-                                partier.Email.Replace(',', ' '),                                                        // 3.  Email
-                                partier.FirstName.Replace(',', ' '),                                                    // 4.  First Name
-                                partier.LastName.Replace(',', ' '),                                                     // 5.  Last Name
-                                partyHost.MarketingSource != null ? partyHost.MarketingSource.Replace(',', ' ') : null, // 6.  Heard About Us
-                                partyHost.CreateDate,                                                                   // 7.  Registration Date
-                                partier.BillingAddress.Address1.Replace(',', ' '),                                      // 8.  Billing Address 1
-                                partier.BillingAddress.Address2.Replace(',', ' '),                                      // 9.  Billing Address 2
-                                partier.BillingAddress.TownCity.Replace(',', ' '),                                      // 10. Billing TownCity
-                                partier.BillingAddress.Postcode.Replace(',', ' '),                                      // 11. Billing Postcode
-                                partyHost.TShirtSize,                                                                   // 12. T-Shirt Size
-                                partier.PartyGuid,                                                                      // 13. Party Guid
-                                partyHost.PartyHeading != null ? partyHost.PartyHeading.Replace(',', ' ') : null,       // 14. Party Title
-                                partyHost.PartyDateTime,                                                                // 15. Party DateTime 
-                                partyHost.PartyAddress.Address1.Replace(',', ' '),                                      // 16. Party Address 1
-                                partyHost.PartyAddress.Address2.Replace(',', ' '),                                      // 17. Party Address 2
-                                partyHost.PartyAddress.TownCity.Replace(',', ' '),                                      // 18. Party TownCity
-                                partyHost.PartyAddress.Postcode.Replace(',', ' '),                                      // 19. Party Postcode
-                                partyHost.PartyCopy != null ? partyHost.PartyCopy.Replace(',', ' ') : null,             // 20. Join Wonderland Story
-                                !string.IsNullOrWhiteSpace(partyHost.PartyImage) ? "Y" : "N",                           // 21. Party Background Image
-                                !string.IsNullOrWhiteSpace(partier.ProfileImage) ? "Y" : "N",                           // 22. Profile Photo
-                                partyHost.FundraisingTarget,                                                            // 23. Fundraising Target
-                                partyHost.SuggestedDonation,                                                            // 24. Suggested Donation
-                                this.Members.GetPartiers(partyHost.PartyGuid).Count(),                                  // 25. Guest Count
-                                partyHost.AmountRaised,                                                                 // 26. Amount Raised
-                                partyHost.Blocked ? "Y" : "N",                                                          // 27. Party Blocked
-                                partier.DotMailerId,                                                                    // 28. DotMailer Id
-                                partier.DotMailerRegistrationComplete ? "Y" : "N",                                      // 29. DotMailer Registration Complete
-                                partyHost.DotMailerPartyPageComplete ? "Y" : "N"                                        // 30. DotMailer Party Page Complete
+                                partier.Id,                                                                                     // 1.  Member Id
+                                PartyHost.Alias,                                                                                // 2.  Member Type
+                                partier.Email.Replace(',', ' '),                                                                // 3.  Email
+                                partier.FirstName.Replace(',', ' '),                                                            // 4.  First Name
+                                partier.LastName.Replace(',', ' '),                                                             // 5.  Last Name
+                                partyHost.MarketingSource != null ? partyHost.MarketingSource.Replace(',', ' ') : string.Empty, // 6.  Heard About Us
+                                partyHost.CreateDate,                                                                           // 7.  Registration Date
+                                partier.BillingAddress.Address1.Replace(',', ' '),                                              // 8.  Billing Address 1
+                                partier.BillingAddress.Address2.Replace(',', ' '),                                              // 9.  Billing Address 2
+                                partier.BillingAddress.TownCity.Replace(',', ' '),                                              // 10. Billing TownCity
+                                partier.BillingAddress.Postcode.Replace(',', ' '),                                              // 11. Billing Postcode
+                                partyHost.TShirtSize,                                                                           // 12. T-Shirt Size
+                                partier.PartyGuid,                                                                              // 13. Party Guid
+                                partyHost.PartyHeading != null ? partyHost.PartyHeading.Replace(',', ' ') : string.Empty,       // 14. Party Title
+                                partyHost.PartyDateTime,                                                                        // 15. Party DateTime 
+                                partyHost.PartyAddress.Address1.Replace(',', ' '),                                              // 16. Party Address 1
+                                partyHost.PartyAddress.Address2.Replace(',', ' '),                                              // 17. Party Address 2
+                                partyHost.PartyAddress.TownCity.Replace(',', ' '),                                              // 18. Party TownCity
+                                partyHost.PartyAddress.Postcode.Replace(',', ' '),                                              // 19. Party Postcode
+                                partyHost.PartyCopy != null ? partyHost.PartyCopy.Replace(',', ' ') : string.Empty,             // 20. Join Wonderland Story
+                                !string.IsNullOrWhiteSpace(partyHost.PartyImage) ? "Y" : "N",                                   // 21. Party Background Image
+                                !string.IsNullOrWhiteSpace(partier.ProfileImage) ? "Y" : "N",                                   // 22. Profile Photo
+                                partyHost.FundraisingTarget,                                                                    // 23. Fundraising Target
+                                partyHost.SuggestedDonation,                                                                    // 24. Suggested Donation
+                                this.Members.GetPartiers(partyHost.PartyGuid).Count(),                                          // 25. Guest Count
+                                partyHost.AmountRaised,                                                                         // 26. Amount Raised
+                                partyHost.Blocked ? "Y" : "N",                                                                  // 27. Party Blocked
+                                partier.DotMailerId,                                                                            // 28. DotMailer Id
+                                partier.DotMailerRegistrationComplete ? "Y" : "N",                                              // 29. DotMailer Registration Complete
+                                partyHost.DotMailerPartyPageComplete ? "Y" : "N"                                                // 30. DotMailer Party Page Complete
                             }
                         )
                     );
@@ -81,31 +81,31 @@ namespace Wonderland.Web.Dashboards
                                 partier.Email.Replace(',', ' '),                                                        // 3.  Email
                                 partier.FirstName.Replace(',', ' '),                                                    // 4.  First Name
                                 partier.LastName.Replace(',', ' '),                                                     // 5.  Last Name
-                                null,                                                                                   // 6.  Heard About Us
+                                string.Empty,                                                                           // 6.  Heard About Us
                                 partyGuest.CreateDate,                                                                  // 7.  Registration Date
                                 partier.BillingAddress.Address1.Replace(',', ' '),                                      // 8.  Billing Address 1
                                 partier.BillingAddress.Address2.Replace(',', ' '),                                      // 9.  Billing Address 2
                                 partier.BillingAddress.TownCity.Replace(',', ' '),                                      // 10. Billing TownCity
                                 partier.BillingAddress.Postcode.Replace(',', ' '),                                      // 11. Billing Postcode
-                                null,                                                                                   // 12. T-Shirt Size
+                                string.Empty,                                                                           // 12. T-Shirt Size
                                 partier.PartyGuid,                                                                      // 13. Party Guid
-                                null,                                                                                   // 14. Party Title
-                                null,                                                                                   // 15. Party DateTime 
-                                null,                                                                                   // 16. Party Address 1
-                                null,                                                                                   // 17. Party Address 2
-                                null,                                                                                   // 18. Party TownCity
-                                null,                                                                                   // 19. Party Postcode
-                                null,                                                                                   // 20. Join Wonderland Story
-                                null,                                                                                   // 21. Party Background Image
+                                string.Empty,                                                                           // 14. Party Title
+                                string.Empty,                                                                           // 15. Party DateTime 
+                                string.Empty,                                                                           // 16. Party Address 1
+                                string.Empty,                                                                           // 17. Party Address 2
+                                string.Empty,                                                                           // 18. Party TownCity
+                                string.Empty,                                                                           // 19. Party Postcode
+                                string.Empty,                                                                           // 20. Join Wonderland Story
+                                string.Empty,                                                                           // 21. Party Background Image
                                 string.IsNullOrWhiteSpace(partier.ProfileImage) ? "Y" : "N",                            // 22. Profile Photo
-                                null,                                                                                   // 23. Fundraising Target
-                                null,                                                                                   // 24. Suggested Donation
-                                null,                                                                                   // 25. Guest Count
-                                null,                                                                                   // 26. Amount Raised
-                                null,                                                                                   // 27. Party Blocked
+                                string.Empty,                                                                           // 23. Fundraising Target
+                                string.Empty,                                                                           // 24. Suggested Donation
+                                string.Empty,                                                                           // 25. Guest Count
+                                string.Empty,                                                                           // 26. Amount Raised
+                                string.Empty,                                                                           // 27. Party Blocked
                                 partier.DotMailerId,                                                                    // 28. DotMailer Id
                                 partier.DotMailerRegistrationComplete ? "Y" : "N",                                      // 29. DotMailer Registration Complete
-                                null                                                                                    // 30. DotMailer Party Page Complete
+                                string.Empty                                                                            // 30. DotMailer Party Page Complete
                             }
                         )
                     );
