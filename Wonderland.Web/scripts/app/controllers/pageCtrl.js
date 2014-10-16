9539c664712db744825114a83bb39ac4ebfb6ebc
MattRawson <rawson.matt@googlemail.com>
2014-10-16 14:44:30 +0100

    - footer links open in new page

--- Wonderland.Web/scripts/app/controllers/pageCtrl.js
+++ Wonderland.Web/scripts/app/controllers/pageCtrl.js
@@ -83,6 +83,14 @@ wonderlandApp.controller('PageCtrl', ['debounce', 'paf', 'safeApply', '$rootScop
     });
 
 
+    /**
+     *
+     * PAGE INACTIVITY TIMEOUT
+     *
+     */
+
+
+
 
 
     /*************************
