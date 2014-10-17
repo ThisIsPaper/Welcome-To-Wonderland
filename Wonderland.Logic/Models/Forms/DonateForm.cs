eb2131c188d64c5ff90f23558e7ba1dc1ec70998
Hendy Racher <git@hendyracher.co.uk>
2014-10-17 11:51:10 +0100

    added validation messages for all mvc forms

--- Wonderland.Logic/Models/Forms/DonateForm.cs
+++ Wonderland.Logic/Models/Forms/DonateForm.cs
@@ -7,37 +7,36 @@ namespace Wonderland.Logic.Models.Forms
 
     public class DonateForm
     {
-        [Required]
+        [Required(ErrorMessage = "Amount Required")]
         [DataType(DataType.Text)]
-        [Range(1, 100000)]
+        [Range(1, 10000, ErrorMessage = "Min £1, Max £10,000")]
         public decimal Amount { get; set; }
 
         //[Display("Allow Gift Aid")]
         public bool AllowGiftAid { get; set; }
 
-        [Required]
-        [MaxLength(20)]
+        [Required(ErrorMessage = "First Name Required")]
+        [MaxLength(20, ErrorMessage = "Max Length of 20")]
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(20)]
+        [Required(ErrorMessage = "Last Name Required")]
+        [MaxLength(20, ErrorMessage = "Max Length of 20")]
         public string LastName { get; set; }
 
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "Address Line 1 Required")]
+        [MaxLength(100, ErrorMessage = "Max Length of 100")]
         public string Address1 { get; set; }
 
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage = "Max Length of 100")]
         public string Address2 { get; set; }
 
-        [Required]
-        [MaxLength(40)]
+        [Required(ErrorMessage = "Town / City Required")]
+        [MaxLength(40, ErrorMessage = "Max Length of 40")]
         public string TownCity { get; set; }
 
-        //public string Country { get; set; }
-
-        [Required]
-        [MaxLength(10)]
+        [Required(ErrorMessage = "Postcode Required")]
+        [MaxLength(10, ErrorMessage = "Max Length of 10")]
+        [DataType(DataType.PostalCode)]
         public string Postcode { get; set; }
 
         [Required]
--- Wonderland.Logic/Models/Forms/RegisterGuestBillingForm.cs
+++ Wonderland.Logic/Models/Forms/RegisterGuestBillingForm.cs
@@ -1,52 +1,49 @@
 ﻿
 namespace Wonderland.Logic.Models.Forms
 {
-
     using System;
     using System.ComponentModel.DataAnnotations;
     using System.Web.Mvc;
 
     public class RegisterGuestBillingForm
     {
-        [Required]
+        [Required(ErrorMessage="Amount Required")]
         [DataType(DataType.Text)]
-        [Range(0, 100000)]
+        [Range(0, 10000, ErrorMessage="Min £0, Max £10,000")]
         public decimal Amount { get; set; }
 
         [DataType(DataType.MultilineText)]
-        [MaxLength(600)]
+        [MaxLength(600, ErrorMessage="Max Length of 600")]
         public string Message { get; set; }
 
         public bool AllowGiftAid { get; set; }
 
-        [Required]
-        [MaxLength(20)]
+        [Required(ErrorMessage="First Name Required")]
+        [MaxLength(20, ErrorMessage="Max Length of 20")]
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(20)]
+        [Required(ErrorMessage="Last Name Required")]
+        [MaxLength(20, ErrorMessage="Max Length of 20")]
         public string LastName { get; set; }
 
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage="Address Line 1 Required")]
+        [MaxLength(100, ErrorMessage="Max Length of 100")]
         public string Address1 { get; set; }
 
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage="Max Length of 100")]
         public string Address2 { get; set; }
 
-        [Required]
-        [MaxLength(40)]
+        [Required(ErrorMessage="Town / City Required")]
+        [MaxLength(40, ErrorMessage="Max Length of 40")]
         public string TownCity { get; set; }
 
-        //public string Country { get; set; }
-
-        [Required]
-        [MaxLength(10)]
+        [Required(ErrorMessage="Postcode Required")]
+        [MaxLength(10, ErrorMessage="Max Length of 10")]
+        [DataType(DataType.PostalCode)]
         public string Postcode { get; set; }
 
         [Required]
         [HiddenInput(DisplayValue = false)]
         public Guid PartyGuid { get; set; }
-
     }
 }
