ba4494ff2bdcdecb243c696f119f1d2cac76d6a8
Hendy Racher <git@hendyracher.co.uk>
2014-10-17 11:24:24 +0100

    validation messages for register guest / host

--- Wonderland.Logic/Models/Forms/RegisterGuestForm.cs
+++ Wonderland.Logic/Models/Forms/RegisterGuestForm.cs
@@ -20,15 +20,15 @@ namespace Wonderland.Logic.Models.Forms
             this.PartyGuid = partyGuid;
         }
 
-        [Required]
-        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage="Required Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage="Invalid Email")]
         //[Remote("ValidateIsEmailAvailable", "RegisterHostSurface", ErrorMessage="Email already registered")]
         [DisplayName("Email Address")]
         public string EmailAddress { get; set; }
 
-        [Required]
+        [Required(ErrorMessage="Required Password")]
         [DataType(DataType.Password)]
-        [MinLength(4)]
+        [MinLength(4, ErrorMessage="Min Length of 4")]
         public string Password { get; set; }
 
         [ScaffoldColumn(false)]
