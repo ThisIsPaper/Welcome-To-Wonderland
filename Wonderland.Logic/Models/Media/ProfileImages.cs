63740d349f3a177628732ba4090241ae3c5dc1c5
Hendy Racher <git@hendyracher.co.uk>
2014-10-21 16:19:32 +0100

    wired up custom party image upload to store in cms media section (new db updates required)

---
+++ Wonderland.Logic/Models/Media/ProfileImages.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Wonderland.Logic.Models.Media
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    internal static class ProfileImages
+    {
+        private const int Id = 5583;
+
+        internal static void CreateProfileImage()
+        {
+
+        }
+    }
+}
