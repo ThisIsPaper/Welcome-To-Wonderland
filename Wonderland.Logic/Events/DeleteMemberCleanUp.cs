a9db2afacb9cbbec4bce9554ffe858379d4ed82f
Hendy Racher <git@hendyracher.co.uk>
2014-10-22 23:35:37 +0100

    event handler to delete party images from media library if a party host is deleted

---
+++ Wonderland.Logic/Events/DeleteMemberCleanUp.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Wonderland.Logic.Events
+{
+    using System.Linq;
+    using System.Web;
+    using Umbraco.Core;
+    using Umbraco.Core.Events;
+    using Umbraco.Core.Models;
+    using Umbraco.Core.Publishing;
+    using Umbraco.Core.Services;
+    using Umbraco.Web;
+    using Wonderland.Logic.Interfaces;
+    using Wonderland.Logic.Models.Members;
+
+    public class DeleteMemberCleanUp : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            MemberService.Deleting += MemberService_Deleting;            
+        }
+
+        private void MemberService_Deleting(IMemberService sender, DeleteEventArgs<IMember> e)
+        {
+            UmbracoHelper umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+
+            foreach(IMember member in e.DeletedEntities)
+            {
+                switch (member.ContentTypeAlias)
+                {
+                    case PartyHost.Alias:
+                    
+                        PartyHost partyHost = (PartyHost)umbracoHelper.TypedMember(member.Id);
+
+                        // remove party image media item
+                        partyHost.PartyImage = null;
+
+                        // delete all guests ?
+
+                        break;
+
+                    default:
+                        
+                        IPartier partier = (IPartier)umbracoHelper.TypedContent(member.Id);
+
+                        // delete media item for profile Image
+                        //partier.ProfileImage = null;
+
+                        // get all wonderlandMessage, remove or mark as deleted ? (media will be on s3, so delete from there)
+                        // remove from wonderlandMemberParty
+
+                        break;
+                }
+            }
+        }
+    }
+}
