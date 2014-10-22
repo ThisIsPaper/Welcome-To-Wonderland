0ac45722fcf875f22f399c1885ca2a43bb00f3b9
Hendy Racher <git@hendyracher.co.uk>
2014-10-22 12:38:04 +0100

    fix for null checks on PartyHost.PartyImage + binaries

--- Wonderland.Logic/Interfaces/IPartyImage.cs
+++ Wonderland.Logic/Interfaces/IPartyImage.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace Wonderland.Logic.Interfaces
 {
+    // we are using an interface as a selected PartyImage could either be a default 'Image' or it could be an uploaded 'PartyImage'
     public interface IPartyImage
     {
         int Id { get; }
