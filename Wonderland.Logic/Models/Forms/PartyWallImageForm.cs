f71510fa1168f1ec5c2e841577dc5f18afe6469d
Hendy Racher <git@hendyracher.co.uk>
2014-10-22 19:10:54 +0100

    hidden field to dispose of orphan wall images

--- Wonderland.Logic/Models/Forms/PartyWallImageForm.cs
+++ Wonderland.Logic/Models/Forms/PartyWallImageForm.cs
@@ -3,11 +3,15 @@ namespace Wonderland.Logic.Models.Forms
 {
     using System.ComponentModel.DataAnnotations;
     using System.Web;
+    using System.Web.Mvc;
 
     public class PartyWallImageForm
     {
         [Required]
         [DataType(DataType.Upload)]
         public HttpPostedFileBase PartyWallImage { get; set; }
+
+        [HiddenInput(DisplayValue=false)]
+        public int? LastPartyWallImageId { get; set; }
     }
 }
