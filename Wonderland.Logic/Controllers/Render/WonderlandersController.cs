222d56f8058aaf3cb40db69fca7822538ebac32e
Hendy Racher <git@hendyracher.co.uk>
2014-10-23 16:03:40 +0100

    caching wonderlanders sql queries (10 min + random 0 to 5 min)

--- Wonderland.Logic/Controllers/Render/WonderlandersController.cs
+++ Wonderland.Logic/Controllers/Render/WonderlandersController.cs
@@ -1,21 +1,49 @@
 ﻿
 namespace Wonderland.Logic.Controllers.Render
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Caching;
     using System.Web.Mvc;
     using Umbraco.Web.Models;
     using Wonderland.Logic.Enums;
     using Wonderland.Logic.Extensions;
     using Wonderland.Logic.Models.Content;
+    using Wonderland.Logic.Models.Members;
 
     public class WonderlandersController : BaseRenderMvcController
     {
-        //[OutputCache(Duration=SitePage.CacheDuration)]
         public override ActionResult Index(RenderModel renderModel)
         {
             Wonderlanders model = (Wonderlanders)renderModel.Content;
-            
-            model.TopFundraisers = this.Members.GetTopPartyHosts(LeaderboardType.TopFundraisers, model.TopFundraisersCount);
-            model.MostGuests = this.Members.GetTopPartyHosts(LeaderboardType.MostGuests, model.MostGuestsCount);
+
+            Cache cache = System.Web.HttpContext.Current.Cache;
+
+            IEnumerable<PartyHost> topFundraisers = (IEnumerable<PartyHost>)cache["TopFundraisers"];
+            IEnumerable<PartyHost> topPartyHosts = (IEnumerable<PartyHost>)cache["TopPartyHosts"];
+
+            if (topFundraisers == null || topPartyHosts == null)
+            {
+                Random random = new Random(); // used to de-sync the cache expiry times
+                random.Next(5);
+
+                if (topFundraisers == null)
+                {
+                    topFundraisers = this.Members.GetTopPartyHosts(LeaderboardType.TopFundraisers, model.TopFundraisersCount);
+
+                    cache.Insert("TopFundraisers", topFundraisers, null, DateTime.UtcNow.AddMinutes(10 + random.Next(5)), Cache.NoSlidingExpiration);
+                }
+
+                if (topPartyHosts == null)
+                {
+                    topPartyHosts = this.Members.GetTopPartyHosts(LeaderboardType.MostGuests, model.MostGuestsCount);
+
+                    cache.Insert("TopPartyHosts", topPartyHosts, null, DateTime.UtcNow.AddMinutes(10 + random.Next(5)), Cache.NoSlidingExpiration);
+                }
+            }
+
+            model.TopFundraisers = topFundraisers;
+            model.MostGuests = topPartyHosts;
 
             return this.CurrentTemplate(model);
         }
