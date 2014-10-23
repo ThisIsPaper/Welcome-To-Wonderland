9b291fb6b1792c968d9a26ef955eafae51e39dbe
MattRawson <rawson.matt@googlemail.com>
2014-10-23 13:45:27 +0100

    - replublished

--- Wonderland.Web/Gruntfile.js
+++ Wonderland.Web/Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
                          uglify: {
                              options: {
                                  mangle: false,
-                                 beautify: true,
+                                 beautify: false,
                                  banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
                                  compress: {
                                      drop_console: false
