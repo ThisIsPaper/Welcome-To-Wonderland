f44ba5fc33d0e7d9f982c05f81e8cce2edf99b61
Hendy Racher <git@hendyracher.co.uk>
2014-10-23 15:42:56 +0100

    caching of sql hit to get total raised on homepage (for 10 min)

--- Wonderland.Logic/Controllers/Render/HomeController.cs
+++ Wonderland.Logic/Controllers/Render/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿
 namespace Wonderland.Logic.Controllers.Render
 {
+    using System;
+    using System.Web.Caching;
     using System.Web.Mvc;
     using Wonderland.Logic.Models.Content;
 
@@ -10,19 +12,30 @@ namespace Wonderland.Logic.Controllers.Render
         public ActionResult Home() // method executed in preference to the Index, as Home is the template name
         {
             Home model = (Home)this.CurrentPage;
+            
+            Cache cache = System.Web.HttpContext.Current.Cache;
 
-            try
+            decimal? totalAmountRaised = (decimal?)cache["TotalAmountRaised"];
+
+            if (!totalAmountRaised.HasValue)
             {
-                model.TotalAmountRaised = this.DatabaseContext.Database.ExecuteScalar<decimal>(@"
-                                                                                                    SELECT      SUM(Amount)
-                                                                                                    FROM        wonderlandDonation
-                                                                                                    WHERE       Success = 1
-                                                                                                ");
+                try
+                {
+                    totalAmountRaised = this.DatabaseContext.Database.ExecuteScalar<decimal>(@"
+                                                                                                SELECT      SUM(Amount)
+                                                                                                FROM        wonderlandDonation
+                                                                                                WHERE       Success = 1
+                                                                                            ");
+                }
+                catch
+                {
+                    totalAmountRaised = 0;
+                }
+
+                cache.Insert("TotalAmountRaised", totalAmountRaised, null, DateTime.UtcNow.AddMinutes(10), Cache.NoSlidingExpiration);
             }
-            catch
-            {
-                model.TotalAmountRaised = 0;
-            }
+
+            model.TotalAmountRaised = totalAmountRaised.Value;
 
             return this.CurrentTemplate(model);
         }
