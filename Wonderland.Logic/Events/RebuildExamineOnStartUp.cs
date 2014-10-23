c55098bcf1641962d1a4685693e50babb7a3b92e
Laurie <lau@voodoobytes.co.uk>
2014-10-23 12:14:31 +0100

    Rebuild examine indexes on application start-up, ensure new instances are clean.

---
+++ Wonderland.Logic/Events/RebuildExamineOnStartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Wonderland.Logic.Events
+{
+		using Examine;
+		using Umbraco.Core;
+		using System.Linq;
+
+    public class RebuildExamineOnStartup : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+						ExamineManager.Instance.IndexProviderCollection.ToList().ForEach(index => index.RebuildIndex());
+        }
+
+    }
+}
