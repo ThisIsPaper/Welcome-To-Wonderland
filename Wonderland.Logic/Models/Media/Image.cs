ec7c840b55c5978ba45b495f041daadff0b225fe
Hendy Racher <git@hendyracher.co.uk>
2014-10-24 16:10:25 +0100

    added AST.S3 reference to web and logic projects

--- Wonderland.Logic/Models/Media/Image.cs
+++ Wonderland.Logic/Models/Media/Image.cs
@@ -5,6 +5,7 @@ namespace Wonderland.Logic.Models.Media
     using Umbraco.Core.Models;
     using Umbraco.Web;
     using Wonderland.Logic.Interfaces;
+    using AST.S3.Helper;
 
     public class Image : SerializablePublishedContentModel, IPartyImage
     {
@@ -25,6 +26,18 @@ namespace Wonderland.Logic.Models.Media
             get
             {
                 return this.GetPropertyValue<string>(Image.AltTextAlias);
+            }
+        }
+
+        public override string Url
+        {
+            get
+            {
+                //UmbracoHelper umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+
+                //return MediaHelper.ParseMedia(this.Id.ToString(), umbracoHelper).Url;
+
+                return base.Url;
             }
         }
     }
