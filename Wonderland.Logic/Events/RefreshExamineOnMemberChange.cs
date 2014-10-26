f5c798336d221ba841c2fb66f44fd23ceee79aa9
Hendy Racher <git@hendyracher.co.uk>
2014-10-26 17:20:22 +0000

    refresh examine on member change & update to 4.5.1

---
+++ Wonderland.Logic/Events/RefreshExamineOnMemberChange.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Wonderland.Logic.Events
+{
+    using AgeBase.ExtendedDistributedCalling.Services;
+    using Umbraco.Core;
+    using Umbraco.Core.Services;
+
+    public class RefreshExamineOnMemberChange : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            MemberService.Created += delegate { ExtendedDistributedCallingService.Refresh(); };
+            MemberService.Deleted += delegate { ExtendedDistributedCallingService.Refresh(); };
+            MemberService.Saved += delegate { ExtendedDistributedCallingService.Refresh(); };
+        }
+    }
+}
