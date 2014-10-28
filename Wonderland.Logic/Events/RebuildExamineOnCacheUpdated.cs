dbc60c7ecd59b7c48a0c63945ed8508b7a4eed8e
Hendy Racher <git@hendyracher.co.uk>
2014-10-28 13:33:59 +0000

    added RebuildExamineOnCacheUpdated event

---
+++ Wonderland.Logic/Events/RebuildExamineOnCacheUpdated.cs
@@ -0,0 +1,23 @@
+﻿namespace Wonderland.Logic.Events
+{
+    using Examine;
+    using System.Linq;
+    using Umbraco.Core;
+    using Umbraco.Web.Cache;
+
+    /// <summary>
+    /// HACK: this is an attempt to ensure content changes via the CMS cause all servers to update their Lucene indexes
+    /// </summary>
+    public class RebuildExamineOnCacheUpdated : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            PageCacheRefresher.CacheUpdated += this.PageCacheRefresher_CacheUpdated;
+        }
+
+        private void PageCacheRefresher_CacheUpdated(PageCacheRefresher sender, Umbraco.Core.Cache.CacheRefresherEventArgs e)
+        {
+            ExamineManager.Instance.IndexProviderCollection.ToList().ForEach(index => index.RebuildIndex());
+        }
+    }
+}
